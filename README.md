94fd794dfef49505c24341d529e3a49991b83a99
Sentiago <arseny317@gmail.com>
2024-10-27 20:10:59 +0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# react-obsidian-setting
---
+++ rollup.config.mjs
@@ -0,0 +1,67 @@
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+
+const baseConfig = {
+    input: 'src/main.ts',
+    external: ['obsidian'],
+    plugins: [
+        json(),
+        nodeResolve({
+            preferBuiltins: true,
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        }),
+        commonjs({
+            include: 'node_modules/**',
+        }),
+        typescript(),
+    ],
+};
+
+const developmentConfig = {
+    ...baseConfig,
+    output: {
+        dir: 'dev',
+        sourcemap: false,
+        format: 'cjs',
+        exports: 'auto',
+    },
+    plugins: [
+        ...baseConfig.plugins,
+    ],
+};
+
+const productionConfig = {
+    ...baseConfig,
+    output: {
+        dir: 'dist',
+        sourcemap: false,
+        sourcemapExcludeSources: true,
+        format: 'cjs',
+        exports: 'auto',
+    },
+    plugins: [
+        ...baseConfig.plugins,
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+            preventAssignment: true,
+        }),
+        terser({
+            compress: true,
+            mangle: true,
+        }),
+        visualizer({
+            open: false,
+            filename: 'bundle-analysis.html',
+        }),
+    ],
+};
+
+const config =
+    process.env.PRODUCTION === '1' ? productionConfig : developmentConfig;
+export default config;
