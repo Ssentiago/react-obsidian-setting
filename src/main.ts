80df83c88bd06ccc0a106e7dcfc09deea55a0d76
Sentiago <arseny317@gmail.com>
2024-11-10 22:30:31 +0300

    refactor: rename file

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ReactObsidianSetting} from './core/ObsidianSettingReact';
+import { ReactObsidianSetting } from './core/ReactObsidianSetting';
 import { MultiDescComponent } from './custom-components/multi-decsription/MultiDescComponent';
 
 export { ReactObsidianSetting, MultiDescComponent };
